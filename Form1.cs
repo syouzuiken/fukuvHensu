d2862806f094af180891c46875a38114ef2adb99
syouzuiken <laybaekzhang@gmail.com>
2022-06-16 14:11:38 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -19,22 +19,26 @@ namespace fukuvHensu
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Left = label1.Left - 10;
+            textBox1.Text = "-5";
+            textBox2.Text = "0";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            label1.Top = label1.Top - 10;
+            textBox1.Text = "0";
+            textBox2.Text = "-5";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            label1.Left = label1.Left + 10;
+            textBox1.Text = "+5";
+            textBox2.Text = "0";
         }
 
         private void button4_Click_1(object sender, EventArgs e)
         {
-            label1.Top = label1.Top + 10;
+            textBox1.Text = "0";
+            textBox2.Text = "+5";
         }
 
         private void timer1_Tick(object sender, EventArgs e)
