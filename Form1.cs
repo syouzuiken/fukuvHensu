e6ee811ed3f7aa6d6902ba0340fb3e3925b98e9c
syouzuiken <laybaekzhang@gmail.com>
2022-06-16 13:38:56 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuvHensu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("章瑞謙");
+        }
+        
+    }
+}
