2bde9f16d2d181e48dd6f70fa89a875a464829b4
syouzuiken <laybaekzhang@gmail.com>
2022-06-16 15:21:27 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+        int vx;
+        int vy;
         public Form1()
         {
             InitializeComponent();
@@ -19,38 +21,38 @@ namespace fukuvHensu
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-5";
-            textBox2.Text = "0";
+            vx += -5;
+            vy += 0;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "-5";
+            vx += 0; 
+            vy += -5; 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "+5";
-            textBox2.Text = "0";
+            vx += 5;
+            vy += 0;
         }
 
         private void button4_Click_1(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "+5";
+            vx += 0;
+            vy += 5;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += int.Parse(textBox1.Text);
-            label1.Top += int.Parse(textBox2.Text);
+            label1.Left += vx;
+            label1.Top += vy;
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("章瑞謙");
-            label1.Visible = false;
+            timer1.Enabled = false;
         }
         
     }
